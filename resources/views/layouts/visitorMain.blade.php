ef9185569c87b4638b0a402446572f43968cb6c1
Sarah M. A <sarahabduh19@gmail.com>
2023-03-08 19:13:56 +0700

    modify visitor layouts

--- resources/views/layouts/visitorMain.blade.php
+++ resources/views/layouts/visitorMain.blade.php
@@ -1,51 +1,54 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	@include('partials.commonStyles')
+	<link rel="stylesheet" href="{{ asset('css/visitorStyle.css') }}">
+
 	<title>{{ $title }} - Web Desa</title>
 </head>
 
 <body>
 	<nav class="navbar navbar-expand-lg bg-light">
 		<div class="container-fluid">
-			<a class="navbar-brand" href="#">Navbar</a>
+			<a class="navbar-brand" href="#">
+				<!-- <img src="" alt="Logo" width="30" height="24" class="d-inline-block align-text-top"> -->
+				Desa Malik
+			</a>
 			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
 			<div class="collapse navbar-collapse" id="navbarTogglerDemo02">
 				<ul class="navbar-nav me-auto mb-2 mb-lg-0">
-					<li class="nav-item">
-						<a class="nav-link active" aria-current="page" href="#">Home</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Link</a>
 					<li class="nav-item dropdown">
 						<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-							Dropdown
+							Profil Desa
 						</a>
 						<ul class="dropdown-menu">
-							<li><a class="dropdown-item" href="#">Action</a></li>
-							<li><a class="dropdown-item" href="#">Another action</a></li>
-							<li>
-								<hr class="dropdown-divider">
-							</li>
-							<li><a class="dropdown-item" href="#">Something else here</a></li>
+							<li><a class="dropdown-item" href="#">Tentang Kami</a></li>
+							<li><a class="dropdown-item" href="#">Demografi Desa</a></li>
 						</ul>
 					</li>
 					<li class="nav-item dropdown">
 						<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-							Dropdown
+							Pemerintahan
 						</a>
 						<ul class="dropdown-menu">
-							<li><a class="dropdown-item" href="#">Action</a></li>
-							<li><a class="dropdown-item" href="#">Another action</a></li>
-							<li>
-								<hr class="dropdown-divider">
-							</li>
-							<li><a class="dropdown-item" href="#">Something else here</a></li>
+							<li><a class="dropdown-item" href="#">Struktur Organisasi</a></li>
+							<li><a class="dropdown-item" href="#">Perangkat Desa</a></li>
+						</ul>
+					</li>
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+							Informasi Publik
+						</a>
+						<ul class="dropdown-menu">
+							<li><a class="dropdown-item" href="#">Agenda Kegiatan</a></li>
+							<li><a class="dropdown-item" href="#">Galeri</a></li>
+							<li><a class="dropdown-item" href="#">Berita Desa</a></li>
 						</ul>
 					</li>
 				</ul>
@@ -56,7 +59,7 @@
 			</div>
 		</div>
 	</nav>
-	{{-- navbar, main content, dan footer --}}
+
 	@yield('main-content')
 	@include('partials.visitorFooter')
 	@include('partials.commonScripts')
