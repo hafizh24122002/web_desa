da50bfee01399f69ea04dc6ef83c107deabfa0ad
Sarah M. A <sarahabduh19@gmail.com>
2023-03-14 14:11:40 +0700

    navbar + header update

--- resources/views/layouts/visitorMain.blade.php
+++ resources/views/layouts/visitorMain.blade.php
@@ -12,7 +12,7 @@
 </head>
 
 <body>
-	<nav class="navbar navbar-expand-lg bg-light">
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 		<div class="container-fluid">
 			<a class="navbar-brand" href="#">
 				<!-- <img src="" alt="Logo" width="30" height="24" class="d-inline-block align-text-top"> -->
@@ -21,7 +21,7 @@
 			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
-			<div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+			<div class="collapse navbar-collapse py-2" id="navbarTogglerDemo02">
 				<ul class="navbar-nav me-auto mb-2 mb-lg-0">
 					<li class="nav-item dropdown">
 						<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -29,6 +29,7 @@
 						</a>
 						<ul class="dropdown-menu">
 							<li><a class="dropdown-item" href="#">Tentang Kami</a></li>
+							<li><a class="dropdown-item" href="#">Geografis Desa</a></li>
 							<li><a class="dropdown-item" href="#">Demografi Desa</a></li>
 						</ul>
 					</li>
@@ -53,8 +54,12 @@
 					</li>
 				</ul>
 				<form class="d-flex" role="search">
-					<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-					<button class="btn btn-outline-success" type="submit">Search</button>
+					<div class="input-group">
+						<input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
+						<button type="button" class="btn btn-secondary rounded" id="searchbtn">
+							<i class="fas fa-search"></i>
+						</button>
+					</div>
 				</form>
 			</div>
 		</div>
