92cd44f34fe0e0d434b92ac1954b861385c09092
meutiajasmine <annisa.mj@gmail.com>
2023-10-05 10:38:56 +0700

    fixed document page

--- resources/views/staf/dokumen/dokumenNew.blade.php
+++ resources/views/staf/dokumen/dokumenNew.blade.php
@@ -4,7 +4,7 @@
 
 <div class="row mt-3 container">
 	<div class="col-lg">
-		<form action="/staf/manajemen-web/dokumen/new-dokumen" method="POST">
+		<form action="/staf/manajemen-web/dokumen/new-dokumen" method="POST" id="form" enctype="multipart/form-data">
 			@csrf
 			<div class="form-group row">
 				<label for="judul" class="col-sm-2 col-form-label">Judul<span style="color:red">*</span></label>
@@ -40,9 +40,10 @@
 				<div class="col-sm-10">
 					<input type="file"
 						class="form-control form-control-sm"
-						name="filename"
+						name="file"
 						placeholder="Isi keterangan dokumen"
                         value="{{ old('filename') }}"
+						accept=".doc,.docx,.pdf"
                         required>
 
 						@error('filename')
@@ -54,19 +55,24 @@
 			</div>
 
             <div class="form-group row">
-				<label for="id_staf" class="col-sm-2 col-form-label">Pengunggah</label>
+				<label for="id_staf" class="col-sm-2 col-form-label">Pengunggah<span style="color:red">*</span></label>
 				<div class="col-sm-10">
-					<select class="form-select form-select-sm" id="grup-input" name="id_staf">
+					<select class="form-select form-select-sm" id="grup-input" name="id_staf" required>
 						<option value="">-- Pilih --</option>
 						@foreach ($staf as $item)
-							<option value="{{ $loop->iteration }}">{{ $item->nama }}</option>
+							<option value="{{ $loop->iteration }}" {{ old('id_staf') == $loop->iteration ? "selected":"" }}>{{ $item->nama }}</option>
 						@endforeach
 					</select>
+					@error('id_staf')
+						<div class="invalid-feedback">
+							{{ $message }}
+						</div>
+					@enderror
 				</div>
 			</div>
 
 			<div class="form-group row">
-				<label for="judul" class="col-sm-2 col-form-label">Status Dokumen</label>
+				<label for="is_active" class="col-sm-2 col-form-label">Status Dokumen</label>
 				<div class="col-sm-10 d-flex align-items-center gap-2">
 					<input class="form-check-input" type="checkbox" id="is_active" name="is_active" value="1" checked>
 					<label class="form-check-label" for="flexCheckDefault">
@@ -78,6 +84,7 @@
 			<div class="d-sm-flex justify-content-md-end">
 				<button class="btn btn-primary mt-2 mb-4 px-3 py-1">Tambah Dokumen Baru</button>
 			</div>
+			<input type="hidden" name="form_reached_controller" value="1">
 		</form>
 	</div>
 </div>
